9f814212762299a7158bcdcd8f55e7b0df1b3e12
tylerjlivingston <66578104+tylerjlivingston@users.noreply.github.com>
2020-06-16 21:19:36 -0400

    Delete AstronomyCalculationTimeJS.js

--- AstronomyCalculationTimeJS.js
+++
@@ -1,8 +0,0 @@
-window.onload=function(){
-    var mb = document.getElementById("test2");
-    mb.addEventListener("click", provideAnswer);
-}
-
-const provideAnswer=function() {
-    alert("TEST WORKED");
-}
