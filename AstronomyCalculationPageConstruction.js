59d5531d5820b8bc97d18dc9e8ff838b324521b7
tylerjlivingston <66578104+tylerjlivingston@users.noreply.github.com>
2020-06-16 21:17:58 -0400

    Add files via upload

---
+++ AstronomyCalculationPageConstruction.js
@@ -0,0 +1,11 @@
+/* 
+Author: Tyler Livingston
+Start date: 6/1/2020
+Updated: 6/4/2020
+Purpose: To calculate common equations related to astronomy
+*/
+
+window.onload=function(){
+    document.getElementById("companyName").innerHTML = "Tyler Livingston 2020"
+    document.getElementById("versionNumber").innerHTML = "Version: 0.1.0"
+}
---
+++ AstronomyCalculationTimeJS.js
@@ -0,0 +1,8 @@
+window.onload=function(){
+    var mb = document.getElementById("test2");
+    mb.addEventListener("click", provideAnswer);
+}
+
+const provideAnswer=function() {
+    alert("TEST WORKED");
+}
