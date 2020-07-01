5ebeca35aacc1a499b239a5ae5ada80f4b565f90
tylerjlivingston <66578104+tylerjlivingston@users.noreply.github.com>
2020-06-30 21:21:02 -0400

    Add files via upload
    Began to use Require.js - organized files based on which screen they belong to.

---
+++ footer.js
@@ -0,0 +1,9 @@
+// Tyler Livingston
+// Created: June 27, 2020
+// Updated: June 27, 2020
+// - Used to edit the footer
+
+define([], function() {
+    document.getElementById("companyName").innerHTML="Tyler Livingston 2020";
+    document.getElementById("versionNumber").innerHTML="Version: 0.1.0";
+});
---
+++ main.js
@@ -0,0 +1,17 @@
+/*
+Tyler Livingston
+Created: June 25, 2020
+Updated: June 28, 2020
+*/
+
+define([
+    'AstroCalcTimeJS/TimingOfSolarEventsJS',
+    'AstroCalcTimeJS/AstronomyCalculationTravelToAnotherStarSystemJS',
+    'AstroCalcPopEquaJS/NewtonsUniversalLawOfGravitationJS',
+    "AstroCalcPopEquaJS/SizeOfEventHorizon",
+    "AstroCalcOtherJS/LightGatheringAbilityJS",
+    "AstroCalcOtherJS/WaitTimeForRadioCommunication",
+    'footer',
+], function() {
+    
+});
